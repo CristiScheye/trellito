5e30a9a2cdb4ca88351c6565fb4460560f5bc3ac
Cristi Scheye <cristi.scheye@gmail.com>
2014-05-19 11:52:52 -0700

    improve style on board show page

--- app/assets/javascripts/views/board_member.js
+++ app/assets/javascripts/views/board_member.js
@@ -1,5 +1,7 @@
 window.Trellito.Views.BoardMemberView = Backbone.View.extend({
   template: JST['boards/_member'],
+  tagName: 'span',
+  className: 'member',
   render: function() {
     var content = this.template({
       member: this.model
